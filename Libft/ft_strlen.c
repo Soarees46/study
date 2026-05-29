caa2c9ea948f87cfb37d4e0a1479959091873251
Carlos Alberto Pinto Soares Ribeiro <carlossoares@MacBook-Air-de-Carlos.local>
2026-05-29 21:33:13 +0100

    correcao strlen

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -6,22 +6,23 @@
 /*   By: carlossoares <carlossoares@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 16:03:48 by calberto          #+#    #+#             */
-/*   Updated: 2026/05/10 16:03:43 by carlossoare      ###   ########.fr       */
+/*   Updated: 2026/05/29 21:32:41 by carlossoare      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strlen(char *s)
+size_t ft_strlen(const char *s)
 {
-    int i;
+    size_t i;
 
     i = 0;
     while (s[i] != '\0')
+    {
         i++;
+    }
     return (i);
 }
-
 /*int main (void)
 {
     printf("Carlos: %d", ft_strlen("Carlos"));
