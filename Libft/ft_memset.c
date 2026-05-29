8dcaa67566990623fce050e15f998513b6ec5198
Carlos Alberto Pinto Soares Ribeiro <carlossoares@MacBook-Air-de-Carlos.local>
2026-05-29 21:16:58 +0100

    modified memset

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -1,1 +1,29 @@
-ß
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carlossoares <carlossoares@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/29 21:14:02 by carlossoare       #+#    #+#             */
+/*   Updated: 2026/05/29 21:14:31 by carlossoare      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+
+void *ft_memset(void *b, int c, size_t len)
+{
+    unsigned char *ptr;
+    size_t i;
+
+    ptr = (unsigned char *)b;
+    i = 0;
+    while (i < len)
+    {
+        ptr[i] = (unsigned char)c;
+        i++;
+    }
+    return (b);
+}
